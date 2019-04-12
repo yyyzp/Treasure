6020e14729faa12f56f180cda10ef3717fa808d1
yangzhipeng <zhipengyang@sohu-inc.com>
2019-04-12 18:30:15 +0800

    user 添加nickname字段

--- app/src/main/java/com/sohu/auto/treasure/entry/User.java
+++ app/src/main/java/com/sohu/auto/treasure/entry/User.java
@@ -72,7 +72,17 @@ public class User {
              */
 
             private String name;
+            private String nickname;
             private String avatar;
+
+
+            public String getNickname() {
+                return nickname;
+            }
+
+            public void setNickname(String nickname) {
+                this.nickname = nickname;
+            }
 
             public String getName() {
                 return name;
